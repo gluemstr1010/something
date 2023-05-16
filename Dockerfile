a8d63356ede64dd12d28c72e977b07563bd419b6
gluemstr1010 <beantotemic@gmail.com>
2023-05-16 20:49:58 +0200

    files pushed

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM php:8.1-fpm
+
+COPY app/ /var/www/html
+
+WORKDIR /var/www/html/
+
+
---
+++ app/php/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+  <html>
+    <head>
+       <title>Nazdar</title>
+    </head>
+        <body>
+	   <h1>This is dockerized app!</h1>
+        </body>
+
+  <html>
